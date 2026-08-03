a63251cb6aa2098e50f32fe88d22ab6ad371d3de
Yaswanth <yash@gmail.com>
2026-08-03 17:28:10 +0530

    Day 2 changes

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/module 2/datatype.py
@@ -0,0 +1,8 @@
+student_name="Yaswanth"
+course_duration=30
+course_rating=4.5
+is_enrolled=True
+print(type(student_name))
+print(type(course_duration))
+print(type(course_rating))
+print(type(is_enrolled))
